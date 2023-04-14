c78c8e1aeffb53dc89fbdbae990731b27eda680a
likui <likui@oschina.cn>
2023-04-14 15:27:35 +0800

    add 001.txt

--- gen.rb
+++ gen.rb
@@ -2,11 +2,13 @@
 
 (1..100).each do |i|
   if (i % 2).zero?
-    name = "%003d.txt" % i
-  else
     name = "%003d.lfs" % i
+  else
+    name = "%003d.txt" % i
   end
-  File.open(name, 'w') do |f|
+  File.open(name, 'a') do |f|
     f.puts name
   end
+  `git add -A`
+  `git commit -m "add #{name}"`
 end
