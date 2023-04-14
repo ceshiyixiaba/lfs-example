5507accbf51b44747eceebd9d950f43d4d2eb644
likui <likui@oschina.cn>
2023-04-14 15:29:07 +0800

    add 001.txt

--- gen.rb
+++ gen.rb
@@ -2,9 +2,9 @@
 
 (1..100).each do |i|
   if (i % 2).zero?
-    name = "%003d.txt" % i
-  else
     name = "%003d.lfs" % i
+  else
+    name = "%003d.txt" % i
   end
   File.open(name, 'a') do |f|
     f.puts name
